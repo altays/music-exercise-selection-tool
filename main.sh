810572959a6837118059fa4fe5602438af76db93
Alex <altays633@gmail.com>
2025-12-01 21:31:41 -0500

    adding recent updates

--- main.sh
+++ main.sh
@@ -4,9 +4,9 @@
 source=""
 sc=data/books/stick-control-gls.json
 ps=data/books/progressive-steps-to-syncopation-tr.json
-mr=data/books/math-rock-primer.json
+# mr=data/books/math-rock-primer.json
 
-selection=$(( ( RANDOM % 3 )  + 1 ))
+selection=$(( ( RANDOM % 2 )  + 1 ))
 NOW=${EPOCHSECONDS}
 
 count=10
@@ -18,57 +18,74 @@ then
 elif [ $selection = "2" ] 
 then
     source=$ps
-elif [ $selection = "3" ] 
-then
-    source=$mr
+# elif [ $selection = "3" ] 
+# then
+#     source=$mr
 else
     echo "Please indicate a valid source."
 fi
 
+
+
 if [ "$1" = "sc" ] 
 then
+
+    echo "=============== " >> ./data/output/idea-output-$NOW.md
    
     while [ $counter -lt $count ]
     do 
-        node index.js a data/books/stick-control-gls.json > ./data/output/output-$NOW.txt
-        echo "==================" >> ./data/output/output-$NOW.txt
 
+        echo "$LS" >> ./data/output/idea-output-$NOW.md
+        node index.js a data/books/stick-control-gls.json > ./data/output/idea-output-$NOW.md
+        echo "$LS" >> ./data/output/idea-output-$NOW.md
+        echo "==================" >> ./data/output/idea-output-$NOW.md
         counter=`expr $counter + 1`
     done
 
 elif [ "$1" = "ps" ]
 then
 
+    echo "==================" >> ./data/output/idea-output-$NOW.md
+
     while [ $counter -lt $count ]
     do 
-        node index.js a data/books/progressive-steps-to-syncopation-tr.json > ./data/output/output-$NOW.txt
-        echo "==================" >> ./data/output/output-$NOW.txt
+        echo "$LS" >> ./data/output/idea-output-$NOW.md
+        node index.js a data/books/progressive-steps-to-syncopation-tr.json > ./data/output/idea-output-$NOW.md
+        echo "$LS" >> ./data/output/idea-output-$NOW.md
+        echo "==================" >> ./data/output/idea-output-$NOW.md
         counter=`expr $counter + 1`
     done
 
 elif [ "$1" = "mr" ]
 then
 
+    echo "==================" >> ./data/output/output-$NOW.md
+
     while [ $counter -lt $count ]
     do 
-        node index.js a data/books/math-rock-primer.json > ./data/output/output-$NOW.txt
-        echo "==================" >> ./data/output/output-$NOW.txt
+        echo "$LS" >> ./data/output/idea-output-$NOW.md
+        node index.js a data/books/math-rock-primer.json > ./data/output/idea-output-$NOW.md
+        echo "$LS" >> ./data/output/idea-output-$NOW.md
+        echo "==================" >> ./data/output/idea-output-$NOW.md
         counter=`expr $counter + 1`
     done
 
 elif [ "$1" = "random" ]
 then
 
+    echo "==================" >> ./data/output/idea-output-$NOW.md
+
     while [ $counter -lt $count ]
     do 
-        echo $source >> ./data/output/output-$NOW.txt
-        node index.js a $source >> ./data/output/output-$NOW.txt
-        echo "==================" >> ./data/output/output-$NOW.txt
+        echo "$LS" >> ./data/output/idea-output-$NOW.md
+        echo $source >> ./data/output/idea-output-$NOW.md
+        node index.js a $source >> ./data/output/idea-output-$NOW.md
+        echo "$LS" >> ./data/output/idea-output-$NOW.md
+        echo "==================" >> ./data/output/idea-output-$NOW.md
         counter=`expr $counter + 1`
     done
 
 else
     echo "Please indicate a valid route."
 fi
-
 
